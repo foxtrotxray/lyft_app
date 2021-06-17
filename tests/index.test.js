d6b79637cffee8ad04a309dff0c2db11bc9714df
Kyle Collins <kyleskycollins@gmail.com>
2021-06-17 15:10:39 -0700

    organized tests for splitting vs routes; test for empty strings

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,18 +10,10 @@ describe('POST /test', function() {
             .expect(400, done);
     });
     it('responds with every third character', function(done) {
-        let input = {string_to_cut: "iamyourlyftdriver"}
         request(server)
             .post('/test')
-            .send(input)
+            .send({string_to_cut: "iamyourlyftdriver"})
             .expect({return_string: "muydv"})
-            .expect('Content-Type', /json/)
-            .expect(200, done);
-        input = {string_to_cut: "brtneh aiinsqm 4xi26svu 3ha4n 4ls8jeqxc nroaeplt14 tcmvre es[ssbwab g0de44"}
-        request(server)
-            .post('/test')
-            .send(input)
-            .expect({return_string: "this is a secret message"})
             .expect('Content-Type', /json/)
             .expect(200, done);
     });
--- tests/spliting_function.test.js
+++ tests/spliting_function.test.js
@@ -2,4 +2,8 @@ const split = require('../splitting_function').pull_third_characters;
 
 test('does it split?', () => {
     expect(split("iamyourlyftdriver")).toBe("muydv");
+    expect(split("brtneh aiinsqm 4xi26svu 3ha4n 4ls8jeqxc nroaeplt14 tcmvre es[ssbwab g0de44")).toBe("this is a secret message");
+})
+test('does it handle empty strings', () => {
+    expect(split("")).toBe("");
 })
