46547f199bcc9b00c0389faee06974dc0ba72b43
Kyle Collins <kyleskycollins@gmail.com>
2021-06-17 15:31:01 -0700

    added tests for special characters & unicode

--- tests/spliting_function.test.js
+++ tests/spliting_function.test.js
@@ -4,6 +4,12 @@ test('does it split?', () => {
     expect(split("iamyourlyftdriver")).toBe("muydv");
     expect(split("brtneh aiinsqm 4xi26svu 3ha4n 4ls8jeqxc nroaeplt14 tcmvre es[ssbwab g0de44")).toBe("this is a secret message");
 })
-test('does it handle empty strings', () => {
+test('does it handle empty strings?', () => {
     expect(split("")).toBe("");
+})
+test('do special characters mess everything up?', () => {
+    expect(split("~!@#$'%'^&*\",.}[]\"[()")).toBe("@'^\"}\")");
+})
+test('unicode 😭😭😭', () => {
+    expect(split("😭😭😭ééé")).toBe("😭é");
 })
